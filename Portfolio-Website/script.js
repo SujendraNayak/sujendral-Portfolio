591906c213364c28b3d4dc8da3a0a5efc0d9229b
SujendraNayak <sujinayak008@gmail.com>
2024-11-06 22:58:43 +0530

    mediaqueries

--- Portfolio-Website/script.js
+++ Portfolio-Website/script.js
@@ -1,8 +1,7 @@
-function toggleMenu() {
+function toggleMenu() 
+{
   const menu = document.querySelector(".menu-links");
-  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
-  hamburgerIcon.classList.toggle("open");
+  icon.classList.toggle("open");
 }
-
-document.querySelector(".hamburger-icon").addEventListener("click", toggleMenu);
